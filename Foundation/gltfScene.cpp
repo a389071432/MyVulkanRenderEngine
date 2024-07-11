9eeb82bf08784a7414d3993ae5587a7a9ba6da88
zzc <zzc_eliza@163.com>
2024-07-11 11:22:01 +0800

    update class gltfScene

--- Foundation/gltfScene.cpp
+++ Foundation/gltfScene.cpp
@@ -22,13 +22,6 @@ namespace zzcVulkanRenderEngine {
         bool ret = loader.LoadASCIIFromFile(&model, &err, &warn, filename);
         ASSERT(ret, "Failed to load glTF!");
 
-        // Create all buffers
-        std::vector<BufferHandle> bufferHandles(model.buffers.size());
-        for (size_t i = 0; i < model.buffers.size(); ++i) {
-
-            bufferHandles[i] = device->createBufferFromData(model.buffers[i].data);
-        }
-
         // Create all images
         std::vector<TextureHandle> textureHandles(model.images.size());
         for (size_t i = 0; i < model.images.size(); ++i) {
@@ -44,57 +37,99 @@ namespace zzcVulkanRenderEngine {
         for (const auto& mesh : model.meshes) {
             Mesh newMesh;
             newMesh.primitive_cnt = mesh.primitives.size();
+            std::vector<float> posData;
+            std::vector<float> normData;
+            std::vector<float> tanData;
+            std::vector<float> uvData;
+            std::vector<uint32_t> indexData;
 
-            // Create a new buffer for this mesh's data
-            std::vector<float> meshData;
             u32 vertexCount = 0;
-
             for (const auto& primitive : mesh.primitives) {
                 const auto& attribs = primitive.attributes;
 
-                // Get accessor for position (assuming it exists)
-                const ::tinygltf::Accessor& posAccessor = model.accessors[attribs.at("POSITION")];
-                vertexCount += posAccessor.count;
+                // Process indices
+                if (primitive.indices >= 0) {
+                    const ::tinygltf::Accessor& accessor = model.accessors[primitive.indices];
+                    uint32_t indexOffset = vertexCount;
 
-                // Interleave attributes
-                for (size_t i = 0; i < posAccessor.count; ++i) {
-                    // Position
-                    for (int j = 0; j < 3; ++j) {
-                        meshData.push_back(GetAttributeValue<float>(model, posAccessor, i, j));
+                    for (size_t i = 0; i < accessor.count; ++i) {
+                        uint32_t index = 0;
+                        switch (accessor.componentType) {
+                        case TINYGLTF_COMPONENT_TYPE_UNSIGNED_SHORT:
+                            index = static_cast<uint32_t>(GetAttributeValue<uint16_t>(model, accessor, i, 0));
+                            break;
+                        case TINYGLTF_COMPONENT_TYPE_UNSIGNED_INT:
+                            index = GetAttributeValue<uint32_t>(model, accessor, i, 0);
+                            break;
+                        case TINYGLTF_COMPONENT_TYPE_UNSIGNED_BYTE:
+                            index = static_cast<uint32_t>(GetAttributeValue<uint8_t>(model, accessor, i, 0));
+                            break;
+                        default:
+                            ASSERT(false, "Unsupported index component type");
+                        }
+                        indexData.push_back(index + indexOffset);
                     }
+                    vertexCount += accessor.count;
+                }
 
-                    // Normal
+                // Gather per-vertex data
+                // Position
+                const ::tinygltf::Accessor& posAccessor = model.accessors[attribs.at("POSITION")];
+                for (size_t i = 0; i < posAccessor.count; ++i) {
+                    for (int j = 0; j < 3; ++j) {
+                        posData.push_back(GetAttributeValue<float>(model, posAccessor, i, j));
+                    }
+                }
+
+                // Normal
+                for (size_t i = 0; i < posAccessor.count; ++i) {
+
                     if (attribs.find("NORMAL") != attribs.end()) {
                         const ::tinygltf::Accessor& accessor = model.accessors[attribs.at("NORMAL")];
                         for (int j = 0; j < 3; ++j) {
-                            meshData.push_back(GetAttributeValue<float>(model, accessor, i, j));
+                            normData.push_back(GetAttributeValue<float>(model, accessor, i, j));
                         }
                     }
+                }
 
-                    // Tangent
+                // Tangent
+                for (size_t i = 0; i < posAccessor.count; ++i) {
+
                     if (attribs.find("TANGENT") != attribs.end()) {
                         const ::tinygltf::Accessor& accessor = model.accessors[attribs.at("TANGENT")];
                         for (int j = 0; j < 4; ++j) {
-                            meshData.push_back(GetAttributeValue<float>(model, accessor, i, j));
+                            tanData.push_back(GetAttributeValue<float>(model, accessor, i, j));
                         }
                     }
+                }
 
-                    // UV
+                // UV
+                for (size_t i = 0; i < posAccessor.count; ++i) {
                     if (attribs.find("TEXCOORD_0") != attribs.end()) {
                         const ::tinygltf::Accessor& accessor = model.accessors[attribs.at("TEXCOORD_0")];
                         for (int j = 0; j < 2; ++j) {
-                            meshData.push_back(GetAttributeValue<float>(model, accessor, i, j));
+                            uvData.push_back(GetAttributeValue<float>(model, accessor, i, j));
                         }
                     }
                 }
             }
 
-            // Create a single buffer for all vertex data
-            //newMesh.vertex_buffer = createBufferFromData(meshData.data());
-
-            // ... (material loading remains the same)
-
+            newMesh.position_buffer = device->createBufferFromData(posData);
+            newMesh.normal_buffer = device->createBufferFromData(normData);
+            newMesh.tangent_buffer = device->createBufferFromData(tanData);
+            newMesh.uv_buffer = device->createBufferFromData(uvData);
+            newMesh.index_buffer = device->createBufferFromData(indexData);
             meshes.push_back(newMesh);
         }
+    }
+
+    template<typename T>
+    T gltfScene::GetAttributeValue(const tinygltf::Model& model, const tinygltf::Accessor& accessor, size_t index, int component) {
+        const tinygltf::BufferView& bufferView = model.bufferViews[accessor.bufferView];
+        const tinygltf::Buffer& buffer = model.buffers[bufferView.buffer];
+        const unsigned char* dataStart = buffer.data.data() + bufferView.byteOffset + accessor.byteOffset;
+        size_t stride = accessor.ByteStride(bufferView);
+        const T* typedData = reinterpret_cast<const T*>(dataStart + index * stride);
+        return typedData[component];
     }
 }
